36e6b1d7f4005579914cb39051c85ea2ebd8b5f6
Robert Rosman <robert.rosman@zmartagroup.com>
2023-01-31 22:31:49 +0100

    fix: move toolbar on window and container resize

--- src/components/ContentEditor.vue
+++ src/components/ContentEditor.vue
@@ -3,7 +3,7 @@ import { ref, onMounted, inject, onUnmounted } from 'vue';
 import Toolbar from './Toolbar.vue';
 import { useStore } from '../composables/useStore'
 import { useVueContentEditor } from '../composables/useVueContentEditor';
-
+import { useResizeObserver } from '@vueuse/core'
 
 const editButtonTop = ref('0')
 const editButtonLeft = ref('0')
@@ -12,6 +12,13 @@ store.contentSource = inject("content-source")
 const editableSelector = "[data-content-block], [data-content-field]"
 
 const { exitEditMode } = useVueContentEditor()
+
+const moveToolbar = (el: HTMLElement) => {
+  const boundaries = el.getBoundingClientRect()
+  const bodyBoundaries = document.body.getBoundingClientRect()
+  editButtonTop.value = `${boundaries.top - bodyBoundaries.top - 43}px`
+  editButtonLeft.value = `${boundaries.left - bodyBoundaries.left}px`
+}
 
 const mouseOverHandler = (e: Event) => {
   if ((e as MouseEvent).shiftKey || store.editMode) {
@@ -24,10 +31,9 @@ const mouseOverHandler = (e: Event) => {
   e.stopPropagation()
   document.querySelectorAll(".vue-content-hovered").forEach(hov => hov.classList.remove("vue-content-hovered"));
   store.activeElement.classList.add("vue-content-hovered")
-  const boundaries = store.activeElement.getBoundingClientRect()
-  const bodyBoundaries = document.body.getBoundingClientRect()
-  editButtonTop.value = `${boundaries.top - bodyBoundaries.top - 43}px`
-  editButtonLeft.value = `${boundaries.left - bodyBoundaries.left}px`
+  moveToolbar(store.activeElement)
+  useResizeObserver(store.activeElement, () => store.activeElement !== undefined && moveToolbar(store.activeElement))
+  window?.addEventListener('resize', () => store.activeElement !== undefined && moveToolbar(store.activeElement))
 }
 
 onMounted(() => {
