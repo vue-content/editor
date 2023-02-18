ff58e4647ebe7b8bf17d26bd0fab96a4c77282d7
Robert Rosman <robert.rosman@zmartagroup.com>
2023-02-18 06:29:07 +0100

    feat: added simple sample for easier development

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # @vue-content/editor
 
 This is the live editor that makes content intractable. Logged in users can hover a block or field and click edit to make changes to the content.
+
+# Development
+
+Run `npm i && npm run dev` to start the local development server. The code that is used for the sample site is located in src/sample. 
---
+++ src/sample/App.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { computed, defineAsyncComponent, inject, ref } from 'vue'
+import { LocalizedInMemorySource } from '@vue-content/core';
+
+const ContentEditor = defineAsyncComponent(async () => {
+  // await import('@vue-content/editor/style.css')
+  const ContentEditor = await import('../components/ContentEditor.vue') // This should be imported from '@vue-content/editor' in real apps
+  return ContentEditor
+})
+
+defineProps<{ msg: string }>()
+const contentSource = inject<LocalizedInMemorySource>("content-source")!
+
+const count = ref(0)
+const doubleCount = computed(() => count.value * 2)
+const editMode = ref(false)
+</script>
+
+<template>
+  <ContentBlock>
+    <h1 v-content-text:title></h1>
+
+    <ContentBlock field="card" class="card">
+      <button type="button" @click="count++" v-content-text:countResult></button>
+      <p v-content-html:editSample></p>
+    </ContentBlock>
+
+    <!-- <ContentList class="paragraph" field="paragraphs">
+      <p v-content-html:value></p>
+    </ContentList> -->
+    <div v-content-html:moreInfo class="paragraphs"></div>
+    <p v-content-text:currentLocale="{ locale: contentSource.locale }"></p>
+  </ContentBlock>
+  <div>
+    <button v-for="locale in contentSource?.locales" @click="contentSource.locale = locale">{{ locale }}</button>
+  </div>
+  <button @click="editMode = !editMode">Toggle edit mode</button>
+  <ContentEditor v-if="editMode"></ContentEditor>
+</template>
+
+<style scoped>
+@import './style.css';
+
+.paragraph:last-of-type p {
+  color: #888;
+}
+</style>
