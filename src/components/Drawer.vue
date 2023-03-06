2614621c061574c7273768384bb17beea0d3e796
Robert Rosman <robert.rosman@zmartagroup.com>
2023-03-06 11:50:37 +0100

    fix: enabled scroll

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,51 +1,50 @@
 <script lang="ts" setup>
-import { onBeforeUnmount, watchEffect } from 'vue'
-import { useWindowSize } from '@vueuse/core'
+import { onBeforeUnmount, watchEffect } from 'vue';
+import { useWindowSize } from '@vueuse/core';
 import { useStore } from '../composables/useStore';
-import BlockForm from './BlockForm.vue'
+import BlockForm from './BlockForm.vue';
 import Breadcrumbs from './Breadcrumbs.vue';
-  
-const { width: windowWidth } = useWindowSize()
-const { store } = useStore()
-store.drawerWidth = Math.max(300, windowWidth.value / 3)
- watchEffect(() => {
-    if (store.drawerWidth > windowWidth.value - 20) {
-        store.drawerWidth = windowWidth.value - 20
-    }
-    store.openDrawer
-        ? document.body.style.marginRight = `${store.drawerWidth}px`
-        : document.body.style.marginRight = "0px"
- })
+
+const { width: windowWidth } = useWindowSize();
+const { store } = useStore();
+store.drawerWidth = Math.max(300, windowWidth.value / 3);
+watchEffect(() => {
+  if (store.drawerWidth > windowWidth.value - 20) {
+    store.drawerWidth = windowWidth.value - 20;
+  }
+  store.openDrawer
+    ? (document.body.style.marginLeft = `${store.drawerWidth}px`)
+    : (document.body.style.marginLeft = '0px');
+});
 
 onBeforeUnmount(() => {
-    document.body.style.marginRight = "0px"
-})
+  document.body.style.marginLeft = '0px';
+});
 </script>
 
 <template>
-    <button @click="store.openDrawer = !store.openDrawer">
-        More
-    </button>
-    <n-drawer
-        v-model:show="store.openDrawer"
-        :width="store.drawerWidth"
-        placement="right"
-        :show-mask="false"
-        :mask-closable="false"
-        resizable
-        :trap-focus="false"
-        :on-update-width="(w: number) => store.drawerWidth = w"
-    >
-      <n-drawer-content title="Content editor" closable>
-        <Breadcrumbs />
-        <n-divider />
-        <BlockForm />
-      </n-drawer-content>
-    </n-drawer>
+  <button @click="store.openDrawer = !store.openDrawer">More</button>
+  <n-drawer
+    v-model:show="store.openDrawer"
+    :width="store.drawerWidth"
+    placement="left"
+    :show-mask="false"
+    :mask-closable="false"
+    resizable
+    :trap-focus="false"
+    :block-scroll="false"
+    :on-update-width="(w: number) => store.drawerWidth = w"
+  >
+    <n-drawer-content title="Content editor" closable>
+      <Breadcrumbs />
+      <n-divider />
+      <BlockForm />
+    </n-drawer-content>
+  </n-drawer>
 </template>
 
 <style>
 body {
-    transition: margin-right 0.3s;
+  transition: margin-left 0.3s;
 }
 </style>
